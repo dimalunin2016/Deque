1caa13d7d86dd7f42f0a972e3f8f931b395edcc1
dimalunin2016 <dimon2016@mail.ru>
2016-11-19 01:26:35 +0400

    Add files via upload

--- deque.h
+++ deque.h
@@ -1,48 +1,55 @@
-#include <bits/stdc++.h>
+//https://github.com/dimalunin2016/Deque
+#include <iostream>
+#include <iterator>
 
-using namespace std;
-template <typename IteratorType>
- class deque_iterator : public iterator<random_access_iterator_tag, IteratorType>
- {
- private:
-
-    IteratorType *it_, *begin_;
+template <class IteratorType, class IteratorType2, class IteratorType3>
+class deque_iterator : public std::iterator<std::random_access_iterator_tag, IteratorType>
+{
+private:
+    IteratorType2 begin_;
     int len_, size_, ind_;
-    void make_step(int t)
-    {
+    void make_step(int t) {
+        if(size_ == 0) {
+            len_ = 0;
+            ind_ = 0;
+            return;
+        }
         len_ += t;
         ind_ += t;
-        while (len_ < 0)
-        {
-            len_ += size_;
+        if (len_ < 0) {
+            int k = 0;
+            k = (0 - len_)/size_ + 1;
+            len_ += k * size_;
         }
         len_ %= size_;
-        it_ = begin_ + len_;
     }
 
- public:
-
-    deque_iterator(IteratorType* a, int position, int ind, int size) : it_(a + position), begin_(a), len_(position), ind_(ind), size_(size) {}
-    deque_iterator(const deque_iterator &it) : it_(it.it_), begin_(it.begin_), len_(it.len_), size_(it.size_), ind_(it.ind_) {}
-    deque_iterator& operator =(const deque_iterator& it) {
-        it_ = it.it_;
+public:
+    deque_iterator() {}
+    deque_iterator(IteratorType2 a, int position, int ind, int size) {
+        begin_ = a;
+        len_ = position;
+        ind_ = ind;
+        size_ = size;
+    }
+    deque_iterator(const deque_iterator &it) {
         begin_ = it.begin_;
         len_ = it.len_;
-        size_ = it.len_;
+        size_ = it.size_;
+        ind_ = it.ind_;
+    }
+    deque_iterator& operator =(const deque_iterator& it) {
+        begin_ = it.begin_;
+        len_ = it.len_;
+        size_ = it.size_;
         ind_ = it.ind_;
         return *this;
     }
-    IteratorType& operator *() {
-        return *it_;
+    IteratorType3 operator *() {
+        return *(begin_ + len_);
     }
-    const IteratorType& operator *() const {
-        return *it_;
-    }
-    IteratorType* operator ->() {
-        return it_;
-    }
-    const IteratorType* operator ->() const {
-        return it_;
+    IteratorType2 operator ->() {
+        return &**this;
     }
     deque_iterator& operator ++() {
         make_step(1);
@@ -62,90 +69,91 @@ template <typename IteratorType>
         --(*this);
         return it;
     }
-    const deque_iterator operator +(const int a) const {
+    deque_iterator operator +(const int a) const {
         deque_iterator it = *this;
         it.make_step(a);
         return it;
     }
-    const deque_iterator operator -(const int a) const {
+    deque_iterator operator -(const int a) const {
         deque_iterator it = *this;
         it.make_step(-a);
         return it;
     }
-    const int operator -(const deque_iterator& it) const {
-        return it_ - it.it_;
+    int operator -(const deque_iterator& it) const {
+        return ind_ - it.ind_;
     }
-    const deque_iterator& operator +=(const int a) {
+    deque_iterator& operator +=(const int a) {
         make_step(a);
         return *this;
     }
-    const deque_iterator& operator -=(const int a) {
+    deque_iterator& operator -=(const int a) {
         make_step(-a);
         return *this;
     }
-    IteratorType& operator [](const int f) {
-        deque_iterator it = *this;
-        it.make_step(f);
-        return *(it.it_);
+    IteratorType3 operator [](const int a) {
+        return *(*this + a);
     }
     bool operator != (const deque_iterator &it) const {
-        return it_ != it.it_;
+        return !(*this == it);
     }
-
     bool operator == (const deque_iterator &it) const {
-        return it_ == it.it_;
+        return (begin_ + len_) == (it.begin_ + it.len_);
     }
-
     bool operator < (const deque_iterator &it) const {
         return ind_ < it.ind_;
     }
     bool operator > (const deque_iterator &it) const {
-        return it < (*this);
+        return ind_ > it.ind_;
     }
     bool operator >= (const deque_iterator &it) const {
-        return (*this > it || *this == it);
+        return ind_ >= it.ind_;
     }
     bool operator <= (const deque_iterator &it) const {
-        return (*this < it || *this == it);
+        return ind_ <= it.ind_;
     }
- };
- /////////////////////////////////////////////////////////////////////////////////////////
+};/////////////////////////////////////////////////////////////////////////////////////////
 template <class T>
- class Deque {
+class Deque {
     int N_, size_;
-    T *a_;
+    T *a_ = nullptr;
     int begin_, end_;
     void reallocator() {
         T *b = new T [2 * N_];
-        for (int i = begin_, j = 0; j < N_; ++j, ++i)  {
-            i %= N_;
-            b[j] = a_[i];
+        for (int j = 0; j < size_; ++j)  {
+            b[j] = (*this)[j];
         }
         delete[] a_;
         a_ = b;
-        end_ = N_;
+        end_ = size_;
         begin_ = 0;
         N_*=2;
     }
     void deletememory() {
         T *b = new T [N_ / 2];
-        int j = 0;
-        for (int i = begin_; j < N_ / 2 && i % N_ != end_; ++j, ++i)  {
-            i %= N_;
-            b[j] = a_[i];
+        for (int j = 0; j < size_; ++j)  {
+            b[j] = (*this)[j];
         }
         delete[] a_;
         a_ = b;
         N_/=2;
-        end_ = j;
+        end_ = size_;
         begin_ = 0;
     }
+    int make_move(const int ch,const int a) const {
+        return (ch + a + N_) % N_;
+    }
 public:
-    typedef deque_iterator<T> iterator;
-    typedef deque_iterator<const T> const_iterator;
+    typedef deque_iterator<T, T*, T&> iterator;
+    typedef deque_iterator<const T, const T*, const T&> const_iterator;
     typedef std::reverse_iterator<iterator> reverse_iterator;
     typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-    Deque() : N_(1), a_(new T [N_]), begin_(0), end_(0), size_(0) {}
+    Deque() {
+        N_ = 8;
+        a_ = new T [N_];
+        begin_ = 0;
+        end_ = 0;
+        size_ = 0;
+    }
     Deque(const Deque &b) {
         N_ = b.N_;
         a_ = new T [N_];
@@ -156,9 +164,12 @@ public:
             a_[i] = b.a_[i];
         }
     }
-    Deque& operator = (const Deque& b)
-    {
+    Deque& operator = (const Deque& b) {
+        if (a_ == b.a_) {
+            return *this;
+        }
         N_ = b.N_;
+        delete[] a_;
         a_ = new T [N_];
         begin_ = b.begin_;
         end_ = b.end_;
@@ -169,33 +180,33 @@ public:
         return *this;
     }
     T &operator [](const int i) {
-        return a_[(begin_ + i) % N_];
+        return a_[make_move(begin_, i)];
     }
     T &back() {
-        return a_[(end_ - 1 + N_) % N_];
+        return a_[make_move(end_, -1)];
     }
     T &front() {
         return a_[begin_];
     }
-    T &operator [](const int i) const{
-        return (*this)[i];
+    const T &operator [](const int i) const{
+        return a_[make_move(begin_, i)];
     }
-    T &front() const {
-        return (*this).front();
+    const T &front() const {
+        return a_[begin_];
     }
-    T &back() const {
-        return (*this).back();
+    const T &back() const {
+        return a_[make_move(end_, -1)];
     }
     void push_back(const T &a) {
         a_[end_] = a;
-        end_ = (end_ + 1) % N_;
+        end_ = make_move(end_, 1);
         ++size_;
         if (end_ == begin_) {
             reallocator();
         }
     }
     void push_front(const T &a) {
-        begin_ = (begin_ - 1 + N_) % N_;
+        begin_ = make_move(begin_, -1);
         a_[begin_] = a;
         ++size_;
         if (end_ == begin_) {
@@ -203,23 +214,15 @@ public:
         }
     }
     void pop_back() {
-        if (end_ == begin_) {
-            size_ = 0;
-            return;
-        }
         --size_;
-        end_ = (end_  - 1 + N_) % N_;
-        if (size_ < N_ / 4 && N_ > 1)
+        end_ = make_move(end_, -1);
+        if (size_ < N_ / 3 && N_ / 2 > 8)
             deletememory();
     }
     void pop_front() {
-        if (end_ == begin_) {
-            size_ = 0;
-            return;
-        }
         --size_;
-        begin_ = (begin_ + 1) % N_;
-        if (size_ < N_ / 4 && N_ > 1)
+        begin_ = make_move(begin_, 1);
+        if (size_ < N_ / 3 && N_ / 2 > 8)
             deletememory();
     }
     int size() const {
@@ -228,53 +231,49 @@ public:
     bool empty() const {
         return size_ ? false : true;
     }
-    iterator begin()
-    {
+    void clear() {
+        begin_ = end_ = 0;
+        size_ = 0;
+        N_ = 8;
+    }
+    ~Deque() {
+        delete[] a_;
+    }
+    iterator begin() {
         return iterator(a_, begin_, 0, N_);
     }
-    const_iterator begin() const
-    {
+    const_iterator begin() const {
         return const_iterator(a_, begin_, 0, N_);
     }
-    const_iterator cbegin() const
-    {
+    const_iterator cbegin() const {
         return const_iterator(a_, begin_, 0, N_);
     }
-    iterator end()
-    {
+    iterator end() {
         return iterator(a_, end_, size_, N_);
     }
-    const_iterator end() const
-    {
+    const_iterator end() const {
         return const_iterator(a_, end_, size_, N_);
     }
-    const_iterator cend() const
-    {
+    const_iterator cend() const {
         return const_iterator(a_, end_, size_, N_);
     }
-    reverse_iterator rbegin()
-    {
-        return reverse_iterator(iterator(a_, end_, size_, N_));
+    reverse_iterator rbegin() {
+        return reverse_iterator(end());
     }
-    const_reverse_iterator rbegin() const
-    {
-        return const_reverse_iterator(iterator(a_, end_, size_, N_));
+    const_reverse_iterator rbegin() const {
+        return const_reverse_iterator(cend());
     }
-    const_reverse_iterator crbegin() const
-    {
-        return const_reverse_iterator(const_iterator(a_, end_, size_, N_));
+    const_reverse_iterator crbegin() const {
+        return const_reverse_iterator(cend());
     }
-    reverse_iterator rend()
-    {
-        return reverse_iterator(iterator(a_, begin_, 0, N_));
+    reverse_iterator rend() {
+        return reverse_iterator(begin());
     }
-    const_reverse_iterator rend() const
-    {
-        return const_reverse_iterator(iterator(a_, begin_, 0, N_));
+    const_reverse_iterator rend() const {
+        return const_reverse_iterator(cbegin());
     }
-    const_reverse_iterator crend() const
-    {
-        return const_reverse_iterator(const_iterator(a_, begin_, 0, N_));
+    const_reverse_iterator crend() const {
+        return const_reverse_iterator(cbegin());
     }
 
- };
+};
